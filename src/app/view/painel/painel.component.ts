510421c5db49bebbbad93c70331d9f6e926dcd19
Matheus Gabriel <matheusg663@gmail.com>
2022-10-25 23:34:21 -0300

    Criação de método e váriaveis para apresentar informações nos quadros do Dashboard

--- src/app/view/painel/painel.component.ts
+++ src/app/view/painel/painel.component.ts
@@ -1,3 +1,6 @@
+import { Animal } from './../../model/animal/animal.model';
+import { AnimalService } from './../../services/animal/animal.service';
+import { SolicitacaoService } from './../../services/solicitacao/solicitacao.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PainelComponent implements OnInit {
 
-  constructor() { }
+  qtdSolicitacao: number = 0;
+  qtdAnimaisAdotados: number = 0;
+  qtdAnimaisAdocao: number = 0;
+
+  constructor(private solicitacaoService: SolicitacaoService, private animalService: AnimalService) { }
 
   ngOnInit(): void {
+    this.quantidadeDeSolicitacoes();
+    this.quantidadeDeAnimaisAdotados();
+    this.quantidadeDeAnimaisAdocao();
   }
 
+  quantidadeDeSolicitacoes(): void {
+    this.solicitacaoService.listaSolicitacoesAdotante(JSON.parse(localStorage.getItem("userInfo")!).id_usuario).subscribe(result => {
+      this.qtdSolicitacao = result.length;
+    });
+  }
+
+  quantidadeDeAnimaisAdotados(): void {
+    this.animalService.procuraAnimalPorTutor(JSON.parse(localStorage.getItem("userInfo")!).id_usuario).subscribe(result => {
+      this.qtdAnimaisAdotados = result.length;
+    });
+  }
+
+  quantidadeDeAnimaisAdocao(): void {
+    this.animalService.procuraAnimalPorDoador(JSON.parse(localStorage.getItem("userInfo")!).id_usuario).subscribe(result => {
+      result.filter(animal => this.countAnimaisAdocao(animal));
+    });
+  }
+
+  countAnimaisAdocao(animal: Animal): void {
+    if(animal.idTutor !== null) {
+      this.qtdAnimaisAdocao++;
+    }
+  }
 }
