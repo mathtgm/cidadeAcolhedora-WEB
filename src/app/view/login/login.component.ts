782b5c129690bc533715e73260547ba86d0d17ab
Matheus Gabriel <matheusg663@gmail.com>
2023-01-02 21:08:11 -0300

    Correção de login

--- src/app/view/login/login.component.ts
+++ src/app/view/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UsuarioService } from 'src/app/services/usuario/usuario.service';
 
@@ -9,20 +10,23 @@ import { UsuarioService } from 'src/app/services/usuario/usuario.service';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private usuarioService: UsuarioService, private router: Router) { }
+  constructor(private usuarioService: UsuarioService, private router: Router, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.formLogin = this.formBuilder.group({
+      usuario: '',
+      senha: ''
+    })
     if(localStorage.getItem("userInfo") !== null) {
       this.router.navigate(["usuario/painel"]);
     }
   }
 
-  user!: String
-  pswd!: String
+  formLogin: FormGroup;
 
   validarUsuario(): void {
     let msg = document.getElementById("notificacao");
-    this.usuarioService.autenticarUsuario(this.user, this.pswd)
+    this.usuarioService.autenticarUsuario(this.formLogin.get("usuario")!.value, this.formLogin.get("senha")!.value)
     .subscribe(result => {
       this.router.navigate(["usuario/painel"]);
       localStorage.setItem("userInfo", JSON.stringify(result))
